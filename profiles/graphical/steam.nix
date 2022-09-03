0e4b5770755c2af8cd53c2bb362074ddb5b9999f
Mattias Evensson <mattias@evensson.eu>
2022-09-03 18:06:16 +0200

    Add steam config from mevensson/devos

---
+++ profiles/graphical/steam.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    glxinfo
+    steam
+  ];
+
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+
+  nixpkgs.config.allowUnfree = true;
+}
