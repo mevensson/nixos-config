0b60f458e017a099b31248e0443f2e35767444e5
Mattias Evensson <mattias@evensson.eu>
2022-09-03 18:36:51 +0200

    Add secrets from mevensson/devos

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,12 @@
 let
-  # set ssh public keys here for your system and user
-  system = "";
-  user = "";
-  allKeys = [ system user ];
+  ryzen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCK3vUzHDlEjCsIHmbNLcOruiFOBVjN8FatW1rFP4R1";
+  t14g1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBFpE56AMvbwB8Wx9NanLIaMbfoNy8sYkrelVzBsCGEA";
+
+  matte = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFK7dAPVafqqETZNEPwtzYVjzrR3pdEqhLSkwAF0J0rH";
+
+  allKeys = [ ryzen t14g1 matte ];
 in
 {
-  "secret.age".publicKeys = allKeys;
+  "matte_password.age".publicKeys = allKeys;
+  "matte_id_ed25519.age".publicKeys = allKeys;
 }
